c23c09d52c9773e34a3bc76ffcb8cebb4747db66
mahmudgalal <Mahmoud.m.galal61@gmail.com>
2024-07-29 21:56:17 +0300

    first commit

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
        <Link to={""}><h1 className='uppercase font-bold text-2xl sm:text-4xl'>Start Framework</h1></Link>
         <button className='md:hidden me-6' onClick={() => menu()}><i className='fa-solid fa-bars '></i></button>
    </div>
-   <div className= {open? "flex flex-col md:flex-row gap-y-7 links mt-9 md:mt-0 transition-all" : "h-0 overflow-hidden transition-all links md:flex md:h-8 md:pe-20"}>
+   <div className= {open? "flex flex-col md:flex-row flex-nowrap gap-y-7 mt-9 overflow-hidden md:mt-0 h-[200px] transition-all duration-700" : "h-0 overflow-hidden transition-all flex gap-y-7 flex-nowrap duration-700 flex-col md:flex-row md:flex md:h-8 md:pe-20"}>
     <NavLink to={"about"} className="text-white px-2 me-3 border rounded border-none py-1 text-base uppercase font-bold">About</NavLink>
     <NavLink to={"portfolio"} className="text-white px-2 me-3 border rounded border-none py-1 text-base uppercase font-bold">Portfolio</NavLink>
     <NavLink to={"contact"} className="text-white px-2 me-3 border rounded border-none py-1 text-base uppercase font-bold">Contact</NavLink>
